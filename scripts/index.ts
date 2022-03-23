65a01c41190c7e0a7e158cf1198fddda115c2e74
axel <axel@wunderapp.es>
2022-03-23 20:43:42 +0100

    borrar logs puesta inicial

--- scripts/index.ts
+++ scripts/index.ts
@@ -24,35 +24,11 @@ document.addEventListener("deviceready", async function () {
 
 $(function () {
 	void cargaInicial();
-	console.log("hola");
-	alert("hola")
+
 });
 
 async function cargaInicial() {
 	General.inicializarMenus();
-
-
-	$(".sliding").on("click", function (event) {
-		console.log("AAAAAAAAAAAA");
-		console.log(Constantes.MODO_LOCAL);
-		void getAlumnos();
-	});
-
 }
 
-async function getAlumnos(): Promise<void> {
-
-	let url = "/alumno/crear";
-	let data = {
-		"nombre": "Axel",
-		"primerApellido": "aaa",
-		"segundoApellido": "bbb"
-	};
-
-	let response = await ComunicacionAjax.sendAjaxRequest("POST", Constantes.URL_API + url, data);
-
-	console.log(response);
-
-
-}
 
