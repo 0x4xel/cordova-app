427b8126f0c80e5da28a36d2591d59f7b87eeacc
axel <axel@wunderapp.es>
2022-03-23 20:42:50 +0100

    Creado Login

---
+++ scripts/login.ts
@@ -0,0 +1,52 @@
+﻿"use strict";
+import { General } from "./funciones";
+import { Constantes } from "./constantes";
+import { ComunicacionAjax } from "./ComunicacionAjax";
+
+$(function () {
+	void cargaInicial();
+	inicializarOnClicks();
+});
+
+async function cargaInicial() {
+
+	$("#txtErrorLogin").hide();
+	$("#txtErrorLogin").removeClass("d-flex");
+
+}
+
+
+function inicializarOnClicks() {
+
+	$(document).on("click", "#btnLogin", function () {
+		void login();
+	});
+
+}
+
+async function login(): Promise<void> {
+
+	let url = "/users/login";
+	let data = {
+		"email": $("#email").val(),
+		"password": $("#password").val(),
+
+	};
+
+
+	let response = await ComunicacionAjax.sendAjaxRequest("POST", Constantes.URL_API + url, data);
+	console.log(response);
+
+	if (response.status == "success") {
+		localStorage.setItem("user_id", response.data.user.id);
+		localStorage.setItem("token", response.data.user.token);
+		window.location.href = "index.html";
+	} else {
+		$("#txtErrorLogin").show();
+		$("#txtErrorLogin").addClass("d-flex");
+		$("#respuestaLogin").text(response.message);
+	}
+	
+
+}
+
